4ffb7dfaa4819df77395b2ded034ad1c0c5af460
Michelle Vuaillat <mv16ae@brocku.ca>
2016-11-07 19:20:31 -0500

    Week9 Lab Challenges 1,2,3

---
+++ week9lab3.js
@@ -0,0 +1,7 @@
+//This will output when the page loads
+document.write("OutputZeroHere");
+
+//This will output when a button is pressed
+function theRest(){
+	document.getElementById("output3").innerHTML="Output Three Here";
+}
