750b5ecb3bae06524728322a575e5a183e774ec4
Michelle Vuaillat <18263749@student.curtin.edu.au>
2016-11-27 13:29:13 -0500

    Javascript Assignment html and js files

---
+++ javascriptassignment.js
@@ -0,0 +1,299 @@
+var alert= alert("Numbers must be in capital letters and no higher than Nine!");
+
+
+function submit(){
+  document.getElementById("output").innerHTML=("Output:");
+}
+
+function add(){
+  var word1= document.getElementById("word1").value;
+  var word2= document.getElementById("word2").value;
+
+
+  if (word1== "One"){
+    var num1 = 1;
+  }
+  else if (word1== "Two"){
+    var num1 = 2;
+  }
+  else if (word1== "Three"){
+      var num1 = 3;
+    }
+    else if (word1== "Four"){
+        var num1 = 4;
+      }
+      else if (word1== "Five"){
+          var num1 = 5;
+        }
+        else if (word1== "Six"){
+            var num1 = 6;
+          }
+          else if (word1== "Seven"){
+              var num1 = 7;
+            }
+            else if (word1== "Eight"){
+                var num1 = 8;
+              }
+              else if (word1== "Nine"){
+                  var num1 = 9;
+                }
+                else {
+                  document.getElementById("output").innerHTML = ("Please choose a lower number!")
+                }
+
+
+if (word2== "One"){
+       var num2 = 1;
+     }
+  else if (word2== "Two"){
+    var num2 = 2;
+  }
+  else if (word2== "Three"){
+    var num2 = 3;
+  }
+  else if (word2== "Four"){
+    var num2 = 4;
+  }
+  else if (word2== "Five"){
+    var num2 = 5;
+  }
+  else if (word2== "Six"){
+    var num2 = 6;
+  }
+  else if (word2== "Seven"){
+    var num2 = 7;
+  }
+  else if (word2== "Eight"){
+    var num2 = 8;
+  }
+  else if (word2== "Nine"){
+    var num2 = 9;
+  }
+  else {
+    document.getElementById("output").innerHTML = ("Please choose a lower number!")
+  }
+
+
+document.getElementById("output").innerHTML = (("Output: " + word1 + " Plus " + word2 + " Equals ") + (num1 + num2));
+
+}
+
+
+function subtract(){
+  var word1= document.getElementById("word1").value;
+  var word2= document.getElementById("word2").value;
+
+
+  if (word1== "One"){
+    var num1 = 1;
+  }
+  else if (word1== "Two"){
+    var num1 = 2;
+  }
+  else if (word1== "Three"){
+      var num1 = 3;
+    }
+    else if (word1== "Four"){
+        var num1 = 4;
+      }
+      else if (word1== "Five"){
+          var num1 = 5;
+        }
+        else if (word1== "Six"){
+            var num1 = 6;
+          }
+          else if (word1== "Seven"){
+              var num1 = 7;
+            }
+            else if (word1== "Eight"){
+                var num1 = 8;
+              }
+              else if (word1== "Nine"){
+                  var num1 = 9;
+                }
+                else {
+                  document.getElementById("output").innerHTML = ("Please choose a lower number!")
+                }
+
+
+if (word2== "One"){
+       var num2 = 1;
+     }
+  else if (word2== "Two"){
+    var num2 = 2;
+  }
+  else if (word2== "Three"){
+    var num2 = 3;
+  }
+  else if (word2== "Four"){
+    var num2 = 4;
+  }
+  else if (word2== "Five"){
+    var num2 = 5;
+  }
+  else if (word2== "Six"){
+    var num2 = 6;
+  }
+  else if (word2== "Seven"){
+    var num2 = 7;
+  }
+  else if (word2== "Eight"){
+    var num2 = 8;
+  }
+  else if (word2== "Nine"){
+    var num2 = 9;
+  }
+  else {
+    document.getElementById("output").innerHTML = ("Please choose a lower number!")
+  }
+
+
+document.getElementById("output").innerHTML = (("Output: " + word1 + " Minus " + word2 + " Equals ") + (num1 - num2));
+
+}
+
+function multiply(){
+  var word1= document.getElementById("word1").value;
+  var word2= document.getElementById("word2").value;
+
+
+  if (word1== "One"){
+    var num1 = 1;
+  }
+  else if (word1== "Two"){
+    var num1 = 2;
+  }
+  else if (word1== "Three"){
+      var num1 = 3;
+    }
+    else if (word1== "Four"){
+        var num1 = 4;
+      }
+      else if (word1== "Five"){
+          var num1 = 5;
+        }
+        else if (word1== "Six"){
+            var num1 = 6;
+          }
+          else if (word1== "Seven"){
+              var num1 = 7;
+            }
+            else if (word1== "Eight"){
+                var num1 = 8;
+              }
+              else if (word1== "Nine"){
+                  var num1 = 9;
+                }
+                else {
+                  document.getElementById("output").innerHTML = ("Please choose a lower number!")
+                }
+
+
+if (word2== "One"){
+       var num2 = 1;
+     }
+  else if (word2== "Two"){
+    var num2 = 2;
+  }
+  else if (word2== "Three"){
+    var num2 = 3;
+  }
+  else if (word2== "Four"){
+    var num2 = 4;
+  }
+  else if (word2== "Five"){
+    var num2 = 5;
+  }
+  else if (word2== "Six"){
+    var num2 = 6;
+  }
+  else if (word2== "Seven"){
+    var num2 = 7;
+  }
+  else if (word2== "Eight"){
+    var num2 = 8;
+  }
+  else if (word2== "Nine"){
+    var num2 = 9;
+  }
+  else {
+    document.getElementById("output").innerHTML = ("Please choose a lower number!")
+  }
+
+
+document.getElementById("output").innerHTML = (("Output: " + word1 + " Times " + word2 + " Equals ") + (num1 * num2));
+
+}
+
+function divide(){
+  var word1= document.getElementById("word1").value;
+  var word2= document.getElementById("word2").value;
+
+
+  if (word1== "One"){
+    var num1 = 1;
+  }
+  else if (word1== "Two"){
+    var num1 = 2;
+  }
+  else if (word1== "Three"){
+      var num1 = 3;
+    }
+    else if (word1== "Four"){
+        var num1 = 4;
+      }
+      else if (word1== "Five"){
+          var num1 = 5;
+        }
+        else if (word1== "Six"){
+            var num1 = 6;
+          }
+          else if (word1== "Seven"){
+              var num1 = 7;
+            }
+            else if (word1== "Eight"){
+                var num1 = 8;
+              }
+              else if (word1== "Nine"){
+                  var num1 = 9;
+                }
+                else {
+                  document.getElementById("output").innerHTML = ("Please choose a lower number!")
+                }
+
+
+if (word2== "One"){
+       var num2 = 1;
+     }
+  else if (word2== "Two"){
+    var num2 = 2;
+  }
+  else if (word2== "Three"){
+    var num2 = 3;
+  }
+  else if (word2== "Four"){
+    var num2 = 4;
+  }
+  else if (word2== "Five"){
+    var num2 = 5;
+  }
+  else if (word2== "Six"){
+    var num2 = 6;
+  }
+  else if (word2== "Seven"){
+    var num2 = 7;
+  }
+  else if (word2== "Eight"){
+    var num2 = 8;
+  }
+  else if (word2== "Nine"){
+    var num2 = 9;
+  }
+  else {
+    document.getElementById("output").innerHTML = ("Please choose a lower number!")
+  }
+
+
+document.getElementById("output").innerHTML = (("Output: " + word1 + " Divided by " + word2 + " Equals ") + (num1 / num2));
+
+}
